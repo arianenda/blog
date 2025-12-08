be87855916cd6694e48127206d45ac6a51a88643
arianenda <athariq@evanarianenda.com>
2025-12-08 13:53:17 +0700

    style: Restyle post lists

--- .eleventy.js
+++ .eleventy.js
@@ -148,6 +148,11 @@ module.exports = function (eleventyConfig) {
     );
   });
 
+  // New filter for "06 June 2020" format
+  eleventyConfig.addFilter("longDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("dd MMMM yyyy");
+  });
+
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
   eleventyConfig.addFilter("htmlDateString", (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
